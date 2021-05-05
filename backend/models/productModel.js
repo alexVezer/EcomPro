d9b8e9ed7280cd97f545738d5d785585cdbfd13f
alexVezer <alexartimon@gmail.com>
2021-05-05 18:24:46 +0300

    data modeling

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,24 +1,66 @@
 import mongoose from 'mongoose'
 
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true},
+    rating: { type: Number, required: true},
+    comment: { type: String, required: true},
+  },
+  {
+    timestamp:true,
+  }
+)
+
 const productSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
   name: {
     type:String,
     required:true
   },
-  email: {
-    type:String,
-    required:true,
-    unique:true
-  },
-  password: {
+  image: {
     type:String,
     required:true
   },
-  isAdmin: {
-    type:Boolean,
+  brand: {
+    type:String,
+    required:true
+  },
+  category: {
+    type:String,
+    required:true
+  },
+  description: {
+    type:String,
+    required:true
+  },
+  reviews:[reviewSchema],
+
+  rating: {
+    type:Number,
     required:true,
-    default: false
-  }
+    default: 0
+  },
+  numReviews: {
+    type:Number,
+    required:true,
+    default: 0
+  },
+
+  price: {
+    type:Number,
+    required:true,
+    default: 0
+  },
+
+  countInStock: {
+    type:Number,
+    required:true,
+    default: 0
+  },
 
 
 },{
@@ -26,6 +68,6 @@ const productSchema = mongoose.Schema({
 } 
 )
 
-const User = mongoose.model('User', userSchema)
+const Product = mongoose.model('Product', productSchema)
 
-export default User
+export default Product
