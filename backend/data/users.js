b303b66f1ee0aadced1c6f9c5015c9eddeb5f247
alexVezer <alexartimon@gmail.com>
2021-05-11 21:04:08 +0300

    Data inserted into database

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,18 +4,18 @@ const users = [
   {
     name: 'Admin User',
     email: 'admin@example.com',
-    pasword: bcrypt.hashSync('123456',10),
+    password: bcrypt.hashSync('123456',10),
     isAdmin: true,
   },
   {
     name: 'John Doe',
     email: 'john@example.com',
-    pasword: bcrypt.hashSync('123456',10),
+    password: bcrypt.hashSync('123456',10),
   },
   {
     name: 'Jane Doe',
     email: 'jane@example.com',
-    pasword: bcrypt.hashSync('123456',10),
+    password: bcrypt.hashSync('123456',10),
   
   },
 ]
