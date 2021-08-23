73792f0bcbcaf660189d81ca3b1f77e8457d9af4
AlexV <alexartimon@gmail.com>
2021-08-23 22:30:11 +0300

    controlers

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -11,8 +11,8 @@ const Header = () => {
       <Navbar.Brand>e-ComPro</Navbar.Brand>
     </LinkContainer>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
+    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+      <Nav className="ml-auto">
       <LinkContainer to='/cart'>
       <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
       </LinkContainer>  
