a92e279547bda4849f54bf88ae1adff58c7fb133
AlexV <alexartimon@gmail.com>
2021-08-24 15:54:38 +0300

    user authentication with jwt

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors'
 import { notFound, errorHandler} from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -11,6 +12,7 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('API is running...')
@@ -18,6 +20,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
