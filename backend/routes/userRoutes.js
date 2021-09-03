94c9dbb11789d31c4ebbb7ecd9ea588a11c60fa8
AlexV <alexartimon@gmail.com>
2021-09-03 22:51:21 +0300

    admin stuff

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, } from '../controllers/userControllers.js'
+import { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserById, updateUser, } from '../controllers/userControllers.js'
 import { admin, protect } from '../middleware/authMiddleware.js'
 
 router
@@ -11,5 +11,9 @@ router
     .route('/profile')
     .get(protect, getUserProfile)
     .put(protect, updateUserProfile)
+router.route('/:id')
+        .delete(protect, admin, deleteUser)
+        .get(protect, admin, getUserById)
+        .put(protect, admin, updateUser)
 
 export default router
--- front-end/src/store.js
+++ front-end/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {productListReducer, productDetailsReducer } from './reducers/productReducers.js'
 import { cartReducer } from './reducers/cartReducers.js'
-import { userLoginReducer, userRegisterReducer , userDetailsReducer, userUpdateProfileReducer, userListReducer} from './reducers/userReducers.js'
+import { userLoginReducer, userRegisterReducer , userDetailsReducer, userUpdateProfileReducer, userListReducer, userDeleteReducer} from './reducers/userReducers.js'
 import { orderCreateReducer, orderDetailsReducer, orderListMyReducer, orderPayReducer } from './reducers/orderReducer.js'
 
 const reducer = combineReducers({ 
@@ -15,6 +15,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   userList: userListReducer,
+  userDelete: userDeleteReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
